4d7db0e957e64135a05b0bea919c3d4a8e84d894
Vini Costa <vc896536@gmail.com>
2023-01-16 23:04:40 -0300

    Service layer, component registration

--- src/main/java/com/springbootjpa/springbootjpa/resources/UserResource.java
+++ src/main/java/com/springbootjpa/springbootjpa/resources/UserResource.java
@@ -1,18 +1,32 @@
 package com.springbootjpa.springbootjpa.resources;
 
 import com.springbootjpa.springbootjpa.entities.User;
+import com.springbootjpa.springbootjpa.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User user = new User(1, "Vini", "vini@gmail.com", "(11) 95338-0525", "123456");
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = userService.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Integer id) {
+        User user = userService.findById(id);
         return ResponseEntity.ok().body(user);
     }
 }
---
+++ src/main/java/com/springbootjpa/springbootjpa/services/UserService.java
@@ -0,0 +1,25 @@
+package com.springbootjpa.springbootjpa.services;
+
+import com.springbootjpa.springbootjpa.entities.User;
+import com.springbootjpa.springbootjpa.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findById(Integer id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        return optionalUser.get();
+    }
+}
